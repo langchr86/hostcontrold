e6d8343e8753f47faeb286d35dcceca21f4d92d8
Christian Lang <lang.chr86@gmail.com>
2014-08-18 20:14:21 +0200

    r Extracted function startServer() and checkAndSignalServerState() r Introduced interval variable. c overworked comments.

--- src/ServerControlDaemon.cpp
+++ src/ServerControlDaemon.cpp
@@ -32,6 +32,8 @@ static const string mServerName("lang-mainserver");
 static const string mIP("192.168.0.9");
 static const string mMAC("00:01:2E:31:64:FF");
 
+static const int interval = 5;
+
 // global constants
 static const string mSep("/");
 static const string mDirectory = mPath + mServerName + mSep;
@@ -44,6 +46,8 @@ static const string mStart("start");
 time_t timer;
 struct tm* timeinfo;
 ofstream logFile;
+int pingRes = 0;
+int serverRunning = -1;
 
 static void log(const char* text) {
 	logFile.open((mDirectory + mLogName).c_str(), ios_base::out | ios_base::app);
@@ -70,6 +74,44 @@ static void logWithInt(const char* text, int num) {
 		logFile.close();
 	} else {
 		return;
+	}
+}
+
+static void startServer() {
+	int wolRes = sendWol(mMAC.c_str());
+	if (wolRes != 0) {
+		logWithInt("[error]\tWOL failed!", wolRes);
+	} else {
+		log("[info]\tWOL sent");
+	}
+}
+
+void checkAndSignalServerState(int newState) {
+	if (newState < 0 || newState > 1) {
+		logWithInt("[error]\tWrong newState!", newState);
+		
+	} else if (newState == 1) {
+		// server changed state to running
+		if (serverRunning != 1) {
+			// write log
+			log("[info]\tserver started");
+			// write on-file and delete off-file
+			createFile((mDirectory + mOn).c_str());
+			remove((mDirectory + mOff).c_str());
+			// set server as running
+			serverRunning = 1;
+		}
+	} else {
+		// server changed state to stopped
+		if (serverRunning != 0) {
+			// write log
+			log("[info]\tserver stopped");
+			// write off-file and delete on-file
+			createFile((mDirectory + mOff).c_str());
+			remove((mDirectory + mOn).c_str());
+			// set server as not running
+			serverRunning = 0;
+		}
 	}
 }
 
@@ -115,56 +157,32 @@ int main(int argc, char* argv[]) {
 	
 	log("[info]\tDaemon started");
 
-	int pingRes = 0;
-	int serverRunning = -1;
 	
 	while (1) {		
-		sleep(5);
+		sleep(interval);
 		
-		// Test if server running
+		// check if server is running
 		pingRes = ping(mIP);
 		
-		if (pingRes > 0) {	
+		if (pingRes > 0) {	// server running
 #if DEBUG
 			log("[debug]\tping > 0");
 #endif			
-			// server running
-			if (serverRunning != 1) {
-				// write log
-				log("server started");
-				// write On-File and delete Off-File
-				createFile((mDirectory + mOn).c_str());
-				remove((mDirectory + mOff).c_str());
-				// Set Server as running
-				serverRunning = 1;
-			}
-		} else if (pingRes == 0) {
+			checkAndSignalServerState(1);
+			
+		} else if (pingRes == 0) {	// server not running
 #if DEBUG
 			log("[debug]\tping == 0");
-#endif			
-			// server not running
-			if (serverRunning != 0) {
-				// write log
-				log("server stopped");
-				// write Off-File and delete On-File
-				createFile((mDirectory + mOff).c_str());
-				remove((mDirectory + mOn).c_str());
-				// Set Server as not running
-				serverRunning = 0;
-			}
+#endif		
+			checkAndSignalServerState(0);
+			
 		} else {
 			logWithInt("[error]\tping failed!", pingRes);
 		}
 		
-		// Test if Start-File
+		// check if start-file around
 		if (checkAndRemoveFile((mDirectory + mStart).c_str()) && serverRunning != 1) {
-			// start server
-			int wolRes = sendWol(mMAC.c_str());
-			if (wolRes != 0) {
-				logWithInt("WOL failed!", wolRes);
-			} else {
-				log("WOL sent");
-			}
+			startServer();
 		}
 	}
 
