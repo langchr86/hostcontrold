48c15b58bb3a1751ce9122f1c4c094f29bbf7f2b
Christian Lang <lang.chr86@gmail.com>
2016-03-29 13:52:27 +0200

    + Add nas08 config.

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char* argv[]) {
 	nas16_config.mac = "40:8D:5C:B6:E6:52";
 	nas16_config.ssh_password = "oaJl|5)Offs6";
 	nas16_config.shutdown_use_ssh = true;
-	nas16_config.control_interval = std::chrono::seconds(10);
+	nas16_config.control_interval = std::chrono::seconds(5);
 	nas16_config.shutdown_timeout = std::chrono::minutes(10);
 
 	ServerControl::ClientList nas16_clients;
@@ -58,9 +58,22 @@ int main(int argc, char* argv[]) {
 	nas16_clients.emplace_back("192.168.0.12", "erdin-velin");
 
 	ServerControl nas16("/share/lang-nas16/", nas16_config, nas16_clients);
+
+	ServerControl::Config nas08_config;
+	nas08_config.ip = "192.168.0.9";
+	nas08_config.mac = "00:01:2E:31:64:FF";
+	nas08_config.ssh_password = "oaJl|5)Offs6";
+	nas08_config.shutdown_use_ssh = true;
+	nas08_config.control_interval = std::chrono::seconds(30);
+	nas08_config.shutdown_timeout = std::chrono::minutes(1);
+
+	ServerControl::ClientList nas08_clients;
+
+	ServerControl nas08("/share/lang-nas08/", nas08_config, nas08_clients);
 	
 	while (true) {
 		nas16.DoWork();
+		nas08.DoWork();
 
 		std::this_thread::sleep_for(seconds(1));
 	}
