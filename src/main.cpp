29d964d8cf9ea6fa22a30b6d7833cd43b8227cfb
Christian Lang <lang.chr86@gmail.com>
2017-04-01 12:44:18 +0200

    - Remove outcommented code for daemon forking.

--- src/main.cpp
+++ src/main.cpp
@@ -8,45 +8,15 @@
 
 
 int main(int argc, char* argv[]) {
-	
-/**********************************
- * Start of Daemon
- * Has to be run in background
- *********************************/
 
-	// setup main logger
+// setup main logger
+//*******************
 	Logger logger("/share/ServerControl.log");
-
-/*
-	//unmasking the file mode
-	umask(0);
-
-	//set new session
-	pid_t sid = setsid();
-	if(sid < 0) {
-		logger.Log("[error]\tsession id failed!");
-		exit(1);
-	}
-	
-	// Change the current working directory to root.
-	if (chdir("/") < 0) {
-		logger.Log("[error]\tchange dir failed!");
-		exit(1);
-	}
-
-	// Close stdin. stdout and stderr
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
-	*/
-	
 	logger.Log("[info]\tDaemon started");
 
 
-/**********************************
- * Main programm
- *********************************/
-
+// main server
+//*************
 	ServerControl::Config nas16_config;
 	nas16_config.ip = "192.168.0.7";
 	nas16_config.mac = "40:8D:5C:B6:E6:52";
@@ -62,6 +32,9 @@ int main(int argc, char* argv[]) {
 
 	ServerControl nas16("/share/lang-nas16/", nas16_config, nas16_clients);
 
+
+// backup server
+//***************
 	ServerControl::Config nas08_config;
 	nas08_config.ip = "192.168.0.9";
 	nas08_config.mac = "00:01:2E:31:64:FF";
@@ -73,7 +46,10 @@ int main(int argc, char* argv[]) {
 	ServerControl::ClientList nas08_clients;
 
 	ServerControl nas08("/share/lang-nas08/", nas08_config, nas08_clients);
-	
+
+
+// work loop
+//***********
 	while (true) {
 		nas16.DoWork();
 		nas08.DoWork();
