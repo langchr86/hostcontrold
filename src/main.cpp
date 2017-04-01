06a48db5bd44fd99d09e7e9aafc7a4dafb30644a
Christian Lang <lang.chr86@gmail.com>
2017-04-01 12:45:10 +0200

    + Update configs for remotes.

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
 // main server
 //*************
 	ServerControl::Config nas16_config;
-	nas16_config.ip = "192.168.0.7";
+	nas16_config.ip = "192.168.0.6";
 	nas16_config.mac = "40:8D:5C:B6:E6:52";
 	nas16_config.ssh_user = "clang";
 	nas16_config.shutdown_use_ssh = true;
@@ -29,6 +29,7 @@ int main(int argc, char* argv[]) {
 	nas16_clients.emplace_back("192.168.0.66", "tv-wohnzimmer");
 	nas16_clients.emplace_back("192.168.0.25", "lang-ct2014");
 	nas16_clients.emplace_back("192.168.0.152", "erdin-velin");
+	nas16_clients.emplace_back("192.168.0.192", "lang-asusnet");
 
 	ServerControl nas16("/share/lang-nas16/", nas16_config, nas16_clients);
 
@@ -36,11 +37,11 @@ int main(int argc, char* argv[]) {
 // backup server
 //***************
 	ServerControl::Config nas08_config;
-	nas08_config.ip = "192.168.0.9";
+	nas08_config.ip = "192.168.0.5";
 	nas08_config.mac = "00:01:2E:31:64:FF";
 	nas08_config.ssh_user = "clang";
 	nas08_config.shutdown_use_ssh = true;
-	nas08_config.control_interval = std::chrono::seconds(30);
+	nas08_config.control_interval = std::chrono::seconds(10);
 	nas08_config.shutdown_timeout = std::chrono::minutes(1);
 
 	ServerControl::ClientList nas08_clients;
