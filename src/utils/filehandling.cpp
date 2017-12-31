1bd4ee9bcfab042ce62d5846cdb03255de3e44e1
Christian Lang <lang.chr86@gmail.com>
2017-12-31 16:23:04 +0100

    - Remove not used file handling functions.

--- src/utils/filehandling.cpp
+++ src/utils/filehandling.cpp
@@ -6,35 +6,6 @@
 
 using namespace std;
 
-
-int removeAllFiles(const char* path) {
-    // These are data types defined in the "dirent" header
-    struct dirent *next_file;
-    DIR *dir;
-
-    char filepath[256];
-
-    dir = opendir(path);
-
-    while ( (next_file = readdir(dir)) )
-    {
-        // build the full path for each file in the folder
-        sprintf(filepath, "%s/%s", path, next_file->d_name);
-        remove(filepath);
-    }
-    return 0;
-}
-
-
-bool checkAndRemoveFile(const char * filepath) {
-	bool check = checkFile(filepath);
-	if (check) {
-		remove(filepath);
-	}
-	return check;
-}
-
-
 bool checkFile(const char * filepath) {
 	ifstream file(filepath);
 	return static_cast<bool>(file);
--- src/utils/filehandling.h
+++ src/utils/filehandling.h
@@ -1,7 +1,5 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int removeAllFiles(const char* path);
-bool checkAndRemoveFile(const char * filepath);
 bool checkFile(const char * filepath);
 bool createFile(const char * filepath);
