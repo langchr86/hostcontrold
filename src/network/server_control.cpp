1d551daef1df363add7b73cac2b7fcb348027e98
Christian Lang <lang.chr86@gmail.com>
2017-12-31 16:19:02 +0100

    c Fix style of ServerControl.

--- src/network/server_control.cpp
+++ src/network/server_control.cpp
@@ -2,9 +2,6 @@
 
 #include <oping.h>
 
-#include <stdio.h>
-#include <string.h>
-
 #include "network/wake_on_lan.h"
 #include "utils/filehandling.h"
 
@@ -17,228 +14,228 @@ const string ServerControl::kFileKeepOn = "force_on";
 const string ServerControl::kFileKeepOff = "force_off";
 
 ServerControl::ServerControl(const string& control_dir, const Config& config, const ClientList& client_list)
-	:	logger_(control_dir + "/log"),
-		control_dir_(control_dir + "/"),
-		config_(config),
-		client_list_(client_list) {
-	// create directory
-	const int status = mkdir(control_dir_.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-	if (status != 0) {
-		// error or already there
-	}
+    : logger_(control_dir + "/log"),
+      control_dir_(control_dir + "/"),
+      config_(config),
+      client_list_(client_list) {
+  // create directory
+  const int status = mkdir(control_dir_.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  if (status != 0) {
+    // error or already there
+  }
 
-	// remove old files
-	remove((control_dir_ + kFileOn).c_str());
-	remove((control_dir_ + kFileOff).c_str());
+  // remove old files
+  remove((control_dir_ + kFileOn).c_str());
+  remove((control_dir_ + kFileOff).c_str());
 
-	// force reset server state to off
-	running_ = true;
-	CheckAndSignalServerState(false);
+  // force reset server state to off
+  running_ = true;
+  CheckAndSignalServerState(false);
 
-	// ensure shutdown timeout will be respected if service starts fresh
-	last_client_ = system_clock::now();
+  // ensure shutdown timeout will be respected if service starts fresh
+  last_client_ = system_clock::now();
 }
 
 ServerControl::~ServerControl() {
 }
 
 void ServerControl::DoWork() {
-	// do work only in defined interval
-	const system_clock::time_point current_time = system_clock::now();
-	if (last_control_ + config_.control_interval > current_time) {
-		return;
-	}
-	last_control_ = current_time;
+  // do work only in defined interval
+  const system_clock::time_point current_time = system_clock::now();
+  if (last_control_ + config_.control_interval > current_time) {
+    return;
+  }
+  last_control_ = current_time;
 
-	// check server state
-	PingServer();
+  // check server state
+  PingServer();
 
-	// check force_on-file and start server if not already running
-	// force_on has higher priority than force_off
-	if (checkFile((control_dir_ + kFileKeepOn).c_str())) {
-		#ifdef DEBUG
-			logger_.Log("[debug]\tforce_on file available");
-		#endif
-		StartServerIfNotRunning();
-		last_client_ = current_time;
-		return;
-	}
+  // check force_on-file and start server if not already running
+  // force_on has higher priority than force_off
+  if (checkFile((control_dir_ + kFileKeepOn).c_str())) {
+#ifdef DEBUG
+    logger_.Log("[debug]\tforce_on file available");
+#endif
+    StartServerIfNotRunning();
+    last_client_ = current_time;
+    return;
+  }
 
-	// check force_off-file and stop server if running
-	if (checkFile((control_dir_ + kFileKeepOff).c_str())) {
-		#ifdef DEBUG
-			logger_.Log("[debug]\tforce_off file available");
-		#endif
-		ShutdownServerIfRunning();
-		return;
-	}
+  // check force_off-file and stop server if running
+  if (checkFile((control_dir_ + kFileKeepOff).c_str())) {
+#ifdef DEBUG
+    logger_.Log("[debug]\tforce_off file available");
+#endif
+    ShutdownServerIfRunning();
+    return;
+  }
 
-	// check if clients around
-	if (CheckClients()) {
-		last_client_ = current_time;
-		StartServerIfNotRunning();
+  // check if clients around
+  if (CheckClients()) {
+    last_client_ = current_time;
+    StartServerIfNotRunning();
 
-	} else {
-		// evaluate if server has to be shutdown
-		if (last_client_ + config_.shutdown_timeout <= current_time) {
-			ShutdownServerIfRunning();
-		}
-	}
+  } else {
+    // evaluate if server has to be shutdown
+    if (last_client_ + config_.shutdown_timeout <= current_time) {
+      ShutdownServerIfRunning();
+    }
+  }
 }
 
 void ServerControl::StartServerIfNotRunning() {
-	// only if server not running
-	if (running_) {
-		return;
-	}
+  // only if server not running
+  if (running_) {
+    return;
+  }
 
-	// send WOL packet
-	if (WakeOnLan::SendWol(config_.mac) == false) {
-		logger_.Log("[error]\tWOL failed!");
-	}
+  // send WOL packet
+  if (WakeOnLan::SendWol(config_.mac) == false) {
+    logger_.Log("[error]\tWOL failed!");
+  }
 
-	#ifdef DEBUG
-		logger_.Log("[debug]\tWOL sent");
-	#endif
+#ifdef DEBUG
+  logger_.Log("[debug]\tWOL sent");
+#endif
 }
 
 void ServerControl::ShutdownServerIfRunning() {
-	if (running_ == false) {
-		return;
-	}
-	ShutdownWithSsh();
+  if (running_ == false) {
+    return;
+  }
+  ShutdownWithSsh();
 }
 
 void ServerControl::ShutdownWithSsh() {
-	// do use SSH if available
-	// This needs a correct hash in ~/.ssh/known_hosts. This can be ensured by
-	// once manually connect via ssh as root to the remote host. In addition the root user needs a not
-	// passphrase secured rsa-key that allowes him to connect to the remote. Ensure to configure
-	// the correct user of the remote.
-	const string ssh_login = string("ssh ")
-			+ config_.ssh_user + string("@") + config_.ip;
-	#ifdef DEBUG
-		logger_.Log("[debug]\tSSH login command: " + ssh_login);
-	#endif
-	FILE *ssh = popen(ssh_login.c_str(), "w");
-	if (ssh == NULL) {
-		logger_.Log("[error]\tpopen failed!");
-		return;
-	}
+  // do use SSH if available
+  // This needs a correct hash in ~/.ssh/known_hosts. This can be ensured by
+  // once manually connect via ssh as root to the remote host. In addition the root user needs a not
+  // passphrase secured rsa-key that allowes him to connect to the remote. Ensure to configure
+  // the correct user of the remote.
+  const string ssh_login = string("ssh ")
+      + config_.ssh_user + string("@") + config_.ip;
+#ifdef DEBUG
+  logger_.Log("[debug]\tSSH login command: " + ssh_login);
+#endif
+  FILE* ssh = popen(ssh_login.c_str(), "w");
+  if (ssh == NULL) {
+    logger_.Log("[error]\tpopen failed!");
+    return;
+  }
 
-	fputs("sudo shutdown -h now", ssh);
+  fputs("sudo shutdown -h now", ssh);
 
-	pclose(ssh);
-	#ifdef DEBUG
-		logger_.Log("[debug]\tshutdown command via SSH executed");
-	#endif
-	return;
+  pclose(ssh);
+#ifdef DEBUG
+  logger_.Log("[debug]\tshutdown command via SSH executed");
+#endif
+  return;
 }
 
 void ServerControl::PingServer() {
-	// check if server is running
-	const int pingRes = Ping(config_.ip);
+  // check if server is running
+  const int pingRes = Ping(config_.ip);
 
-	// server running
-	if (pingRes > 0) {
-		#ifdef DEBUG
-			logger_.Log("[debug]\tserver-ping > 0:\t" + config_.ip);
-		#endif
-		CheckAndSignalServerState(true);
+  // server running
+  if (pingRes > 0) {
+#ifdef DEBUG
+    logger_.Log("[debug]\tserver-ping > 0:\t" + config_.ip);
+#endif
+    CheckAndSignalServerState(true);
 
-	// server not running
-	} else if (pingRes == 0) {
-		#ifdef DEBUG
-			logger_.Log("[debug]\tserver-ping == 0:\t" + config_.ip);
-		#endif
-		CheckAndSignalServerState(false);
+    // server not running
+  } else if (pingRes == 0) {
+#ifdef DEBUG
+    logger_.Log("[debug]\tserver-ping == 0:\t" + config_.ip);
+#endif
+    CheckAndSignalServerState(false);
 
-	// log failed ping
-	} else {
-		logger_.LogWithInt("[error]\tserver-ping failed!:\t" + config_.ip, pingRes);
-	}
+    // log failed ping
+  } else {
+    logger_.LogWithInt("[error]\tserver-ping failed!:\t" + config_.ip, pingRes);
+  }
 }
 
 bool ServerControl::CheckClients() {
-	// check if any client is runnning
-	for (ClientList::const_iterator it = client_list_.begin(); it != client_list_.end(); ++it) {
-		const int pingRes = Ping(it->ip);
+  // check if any client is runnning
+  for (ClientList::const_iterator it = client_list_.begin(); it != client_list_.end(); ++it) {
+    const int pingRes = Ping(it->ip);
 
-		// client answer
-		if (pingRes > 0) {
-			#ifdef DEBUG
-				logger_.Log("[debug]\tclient-ping > 0:\t" + it->description);
-				logger_.Log("skip other pings");
-			#endif
-			return true;
+    // client answer
+    if (pingRes > 0) {
+#ifdef DEBUG
+      logger_.Log("[debug]\tclient-ping > 0:\t" + it->description);
+      logger_.Log("skip other pings");
+#endif
+      return true;
 
-		// no answer
-		} else if (pingRes == 0) {
-			#ifdef DEBUG
-				logger_.Log("[debug]\tclient-ping == 0:\t" + it->description);
-			#endif
+      // no answer
+    } else if (pingRes == 0) {
+#ifdef DEBUG
+      logger_.Log("[debug]\tclient-ping == 0:\t" + it->description);
+#endif
 
-		// log failed ping
-		} else {
-			logger_.LogWithInt("[error]\tclient-ping failed!:\t" + it->description, pingRes);
-		}
-	}
+      // log failed ping
+    } else {
+      logger_.LogWithInt("[error]\tclient-ping failed!:\t" + it->description, pingRes);
+    }
+  }
 
-	// if no clients are running
-	return false;
+  // if no clients are running
+  return false;
 }
 
 void ServerControl::CheckAndSignalServerState(const bool& newState) {
-	if (newState) {
-		// server changed state to running
-		if (running_ == false) {
-			logger_.Log("[info]\tserver started");
+  if (newState) {
+    // server changed state to running
+    if (running_ == false) {
+      logger_.Log("[info]\tserver started");
 
-			// write on-file and delete off-file
-			createFile((control_dir_ + kFileOn).c_str());
-			remove((control_dir_ + kFileOff).c_str());
-			running_ = true;
-		}
-	} else {
-		// server changed state to stopped
-		if (running_) {
-			logger_.Log("[info]\tserver stopped");
+      // write on-file and delete off-file
+      createFile((control_dir_ + kFileOn).c_str());
+      remove((control_dir_ + kFileOff).c_str());
+      running_ = true;
+    }
+  } else {
+    // server changed state to stopped
+    if (running_) {
+      logger_.Log("[info]\tserver stopped");
 
-			// write off-file and delete on-file
-			createFile((control_dir_ + kFileOff).c_str());
-			remove((control_dir_ + kFileOn).c_str());
-			running_ = false;
-		}
-	}
+      // write off-file and delete on-file
+      createFile((control_dir_ + kFileOff).c_str());
+      remove((control_dir_ + kFileOn).c_str());
+      running_ = false;
+    }
+  }
 }
 
 int ServerControl::Ping(const std::string& ip) const {
   // create object
-  pingobj_t * obj = ping_construct();
+  pingobj_t* obj = ping_construct();
 
   // add host to object
   if (ping_host_add(obj, ip.c_str()) < 0) {
-    return -1;	// error
+    return -1;    // error
   }
 
   // send ICMP
   int res = ping_send(obj);
   if (res < 0) {
-    return -2;	// error
+    return -2;    // error
   } else if (res == 0) {
     // no echo replies
   }
 
   // receive info
-  pingobj_iter_t *iter = ping_iterator_get(obj);
+  pingobj_iter_t* iter = ping_iterator_get(obj);
   double latency = -1.0;
   size_t buffer_len = sizeof(latency);
 
   if (iter == NULL) {
-    return -4;	// error
+    return -4;    // error
   }
-  if (ping_iterator_get_info (iter, PING_INFO_LATENCY, &latency, &buffer_len) < 0) {
-    return -5;	// error
+  if (ping_iterator_get_info(iter, PING_INFO_LATENCY, &latency, &buffer_len) < 0) {
+    return -5;    // error
   }
 
   // delete ressources
@@ -246,9 +243,9 @@ int ServerControl::Ping(const std::string& ip) const {
 
   // return result
   if (latency < 0.0) {
-    return 0;	// timeout
+    return 0;    // timeout
   } else {
-    return 1;	// ping reply recieved
+    return 1;    // ping reply recieved
   }
 }
 
--- src/network/server_control.h
+++ src/network/server_control.h
@@ -15,89 +15,89 @@ using std::chrono::seconds;
 using std::chrono::system_clock;
 
 class ServerControl {
-	static const int kServerPort;
-	static const string kFileOn;
-	static const string kFileOff;
-	static const string kFileKeepOn;
-	static const string kFileKeepOff;
+  static const int kServerPort;
+  static const string kFileOn;
+  static const string kFileOff;
+  static const string kFileKeepOn;
+  static const string kFileKeepOff;
 
-public:
-	struct Config {
-		string ip;
-		string mac;
-		string ssh_user;
-		seconds control_interval;
-		seconds shutdown_timeout;
+ public:
+  struct Config {
+    string ip;
+    string mac;
+    string ssh_user;
+    seconds control_interval;
+    seconds shutdown_timeout;
 
-	};
+  };
 
-	struct Machine {
-		Machine(const string& i, const string& d)
-			: ip(i), description(d) {
-		}
-		string ip;
-		string description;
-	};
+  struct Machine {
+    Machine(const string& i, const string& d)
+        : ip(i), description(d) {
+    }
+    string ip;
+    string description;
+  };
 
-	typedef vector<Machine> ClientList;
+  typedef vector<Machine> ClientList;
 
-	ServerControl(const string& control_dir, const Config& config, const ClientList& client_list);
-	virtual ~ServerControl();
+  ServerControl(const string& control_dir, const Config& config, const ClientList& client_list);
+  virtual ~ServerControl();
 
-	/**
-	 * \brief The main work method for each instance. The method returns after each iteration.
-	 */
-	void DoWork();
+  /**
+   * \brief The main work method for each instance. The method returns after each iteration.
+   */
+  void DoWork();
 
-private:
-	Logger logger_;
+ private:
+  Logger logger_;
 
-	const string control_dir_;
-	const Config config_;
-	const ClientList client_list_;
+  const string control_dir_;
+  const Config config_;
+  const ClientList client_list_;
 
-	bool running_;
+  bool running_;
 
-	system_clock::time_point last_control_;
-	system_clock::time_point last_client_;
+  system_clock::time_point last_control_;
+  system_clock::time_point last_client_;
 
-	/*
-	 *	\brief	Method uses a WOL-package to start the server.
-	 */
-	void StartServerIfNotRunning();
+  /*
+   *	\brief	Method uses a WOL-package to start the server.
+   */
+  void StartServerIfNotRunning();
 
-	/*
-	 *	\brief	Method uses a UDP-package to indicate the ShutdownDaemon on the server to shutdown the host.
-	 */
-	void ShutdownServerIfRunning();
+  /*
+   *	\brief	Method uses a UDP-package to indicate the ShutdownDaemon on the server to shutdown the host.
+   */
+  void ShutdownServerIfRunning();
 
-	/**
-	 * \brief	Do use SSH for remote shutdown.
-	 *
-	 * 	This needs a correct hash in ~/.ssh/known_hosts. This can be ensured by
-	 * 	once manually connect via ssh as root to the remote host.
-	 */
-	void ShutdownWithSsh();
+  /**
+   * \brief	Do use SSH for remote shutdown.
+   *
+   * 	This needs a correct hash in ~/.ssh/known_hosts. This can be ensured by
+   * 	once manually connect via ssh as root to the remote host.
+   */
+  void ShutdownWithSsh();
 
-	/**
-	 * \brief Evaluate server state.
-	 */
-	void PingServer();
+  /**
+   * \brief Evaluate server state.
+   */
+  void PingServer();
 
-	/**
-	 * \brief Try to ping all clients and return true if any client could be reached.
-	 */
-	bool CheckClients();
+  /**
+   * \brief Try to ping all clients and return true if any client could be reached.
+   */
+  bool CheckClients();
 
-	/*
-	 *	\brief	Checks if state of server has changed and handles the
-	 *			indicator files and logging of the server state.
-	 *
-	 *	\param	newState	The current state of the server at call time.
-	 */
-	void CheckAndSignalServerState(const bool& newState);
+  /*
+   *	\brief	Checks if state of server has changed and handles the
+   *			indicator files and logging of the server state.
+   *
+   *	\param	newState	The current state of the server at call time.
+   */
+  void CheckAndSignalServerState(const bool& newState);
 
-    int Ping(const std::string& ip) const;
+  int Ping(const std::string& ip) const;
 };
 
 #endif /* SRC_SERVER_CONTROL_H_ */
